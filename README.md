9f5c29d7157e140524e26c3601e22942c487178f
Leonardo <leonardomanrique43@gmail.com>
2019-02-10 03:59:33 -0500

    Revert "Merge branch 'master' of https://github.com/xlxaxmxsx/USIL-20172-TS2-G4"
    This reverts commit 1e16528a85d4569c86977dc57a8c1a53857a377b, reversing
    changes made to 80ccec6095c8b12bfc5ee529e3d29fe55c77b81e.

---
+++ README.md
@@ -0,0 +1,10 @@
+# USIL-20172-TS2-G4
+
+# Pagina web
+
+# Integrantes del proyecto
+
+* Leonardo Manrique
+* Marcelo Baca
+* Luis Alzamora
+* Edwin Canales
---
+++ script.js
@@ -0,0 +1,10 @@
+function upFunction(){
+	alert("Gracias por tu aporte!");
+};
+
+function downFunction(){
+	var feedback = prompt("¿Qué falla en el código?");
+	if (feedback) {
+		alert("Gracias por tu aporte!");
+	}
+};
